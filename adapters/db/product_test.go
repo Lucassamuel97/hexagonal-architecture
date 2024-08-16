4e0575590daeea22bb19ecf2bec5ed9b1463e25f
Lucassamuel97 <lukassamuka88@gmail.com>
2024-08-16 14:36:30 -0300

    implemented save method

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/Lucassamuel97/go-hexagonal/adapters/db"
+	"github.com/Lucassamuel97/go-hexagonal/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -49,4 +50,30 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Product Test", product.GetName())
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+	product.Name = "Product Update"
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, "Product Update", productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
 }
